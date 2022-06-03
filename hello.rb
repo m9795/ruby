a2242004b6ec8fd6358cbeb2feee280b6e0455db
m9795 <106650955+m9795@users.noreply.github.com>
2022-06-03 13:01:48 +0900

    [Uodate]elsif,else

--- hello.rb
+++ hello.rb
@@ -1,32 +1,9 @@
-total = 100
-if total < 200
-  puts "合計点数は200未満です"
-end
+apple = "Yamanashi"
 
-if total >= 150
-  puts "合計は150以上です"
-end
-
-
-hand = "グー"
-if hand == "グー"
-  puts "出した手はグーです"
-end
-
-if hand != "チョキ"
-  puts "出した手はチョキではありません"
-end
-
-if (hand == "グー") || (hand == "パー")
-  puts "出した手はグーまたはパーです"
-end
-
-score = 70
-
-if (score >=50 || score <= 100) && score >= 80
-  puts "得点は50点以上または100点以下で、かつ80点以上です。"
-end
-
-if score >= 50 || (score <= 100 && score >= 80)
-  puts "得点は50点以上、または80点以上で100点以下です。"
+if apple == "Aomori"
+  puts "このリンゴは青森県産です。"
+elsif apple == "Nagano"
+  puts "このリンゴは青森県産ではなく、長野県産です。"
+else
+  puts "このリンゴは青森県産でも長野県産でもありません。"
 end
