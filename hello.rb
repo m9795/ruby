2cefa2e85973953b96f865053cbf42a31d4285c4
m9795 <106650955+m9795@users.noreply.github.com>
2022-06-03 09:56:26 +0900

    [Update]文字列と変数展開

--- hello.rb
+++ hello.rb
@@ -1,7 +1,6 @@
-puts 100
-puts 100 + 3
-puts 100 - 3
-puts 100 * 3
-puts 100 / 3
-puts 100 % 3
-　
+name = "A"
+weight = 50
+
+puts name + "さんの体重は" + weight.to_s + "kgです"
+puts "#{name}さんの体重は#{weight}kgです"
+puts '#{name}さんの体重は#{weight}kgです'
