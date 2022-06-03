60f179cd50dabc35c93c83486073606405841565
m9795 <106650955+m9795@users.noreply.github.com>
2022-06-03 12:58:48 +0900

    [Update]if文

--- hello.rb
+++ hello.rb
@@ -1,6 +1,32 @@
-name = "A"
-weight = 50
+total = 100
+if total < 200
+  puts "合計点数は200未満です"
+end
 
-puts name + "さんの体重は" + weight.to_s + "kgです"
-puts "#{name}さんの体重は#{weight}kgです"
-puts '#{name}さんの体重は#{weight}kgです'
+if total >= 150
+  puts "合計は150以上です"
+end
+
+
+hand = "グー"
+if hand == "グー"
+  puts "出した手はグーです"
+end
+
+if hand != "チョキ"
+  puts "出した手はチョキではありません"
+end
+
+if (hand == "グー") || (hand == "パー")
+  puts "出した手はグーまたはパーです"
+end
+
+score = 70
+
+if (score >=50 || score <= 100) && score >= 80
+  puts "得点は50点以上または100点以下で、かつ80点以上です。"
+end
+
+if score >= 50 || (score <= 100 && score >= 80)
+  puts "得点は50点以上、または80点以上で100点以下です。"
+end
